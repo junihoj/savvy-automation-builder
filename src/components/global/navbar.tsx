f544bfead8d1ac1eb2824570068423bf867c1a6c
junihoj <ezeonyekachukwu98@gmail.com>
2024-07-17 11:47:12 +0100

    initial:missed

---
+++ src/components/global/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = async () => {
+  return (
+   <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
+        <aside className="flex items-center gap-[2px]">
+            <p className='text-3xl font-bold'> FU</p>
+        </aside>
+   </header>
+  )
+}
+
+export default Navbar
