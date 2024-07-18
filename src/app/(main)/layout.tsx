cb1ed964521a09af3f441b8bbe35ef458351970f
junihoj <ezeonyekachukwu98@gmail.com>
2024-07-18 12:10:34 +0100

    added clerk authenticatin

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,17 @@
+import InfoBar from '@/components/infobar'
+import SideBar from '@/components/sidebar'
+import React from 'react'
+type Props = { children: React.ReactNode }
+const layout = (props: Props) => {
+    return (
+        <div className="flex overflow-hidden h-screen">
+            <SideBar />
+            <div className="w-full">
+                <InfoBar />
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+export default layout
